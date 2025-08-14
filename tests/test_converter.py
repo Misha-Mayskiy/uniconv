d06d01e886faf3a1643813d6d300c4d72320536b
Misha-Mayskiy <Shofer2007@gmail.com>
2025-08-14 16:00:18 +0500

    fix: package library change

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from library import UnitConverter, UnknownUnitError, UnknownParameterError
+from uniconv import UnitConverter, UnknownUnitError, UnknownParameterError
 
 
 class TestUnitConverter(unittest.TestCase):
